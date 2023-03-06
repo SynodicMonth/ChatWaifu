f77476fb78e43ca776851d813e83327d14bf4811
SynodicMonth <Synodic_Month@163.com>
2023-03-07 00:57:31 +0800

    feat(presets): implement character cange

--- interface.py
+++ interface.py
@@ -8,6 +8,7 @@ import requests
 import io
 import base64
 import yaml
+import os
 
 # put your openai api key in openai_api_key.txt
 openai.api_key = open("./openai_api_key.txt", "r").read().strip()
@@ -39,13 +40,28 @@ print("hello world")
 max_interaction = 5
 
 # notice to be displayed in the interface
-notice = "**工程早期测试阶段alpha-0.3，GPT回复时间大概5s，绘图约为0.5s/step，请耐心等待，不要多次发送，Markdown功能正在开发中，敬请期待。**"
+notice = "**工程早期测试阶段alpha-0.4，GPT回复时间大概5s，绘图约为0.5s/step，请耐心等待，不要多次发送，代码高亮已经实现，latex还未实现，敬请期待。**"
 # additional css file path
 additional_css = "./additional.css"
+presets = {}
+
+def load_presets():
+    # if there is a directory named "presets", load all the presets
+    if not os.path.exists("./presets"):
+        raise Exception("no presets directory found")
+    else:
+        for file in os.listdir("./presets"):
+            if file.endswith(".yaml"):
+                with open(f"./presets/{file}", "r", encoding='utf-8') as f:
+                    yaml_content = yaml.load(f, Loader=yaml.FullLoader)
+                    presets[yaml_content["id"]] = yaml_content
+        if len(presets) == 0:
+            raise Exception("no presets found, please put yaml files in the presets directory")
+    print(f"loaded {len(presets)} presets")
 
 def ask_diffusion(description, config):
-    prompt = config["diffusion_positive_prompt"] + '(((' + description + ')))'
-    negative_prompt = config["diffusion_negative_prompt"]
+    prompt = presets[config["character_preset"]]["diffusion_positive_prompt"] + '(((' + description + ')))'
+    negative_prompt = presets[config["character_preset"]]["diffusion_negative_prompt"]
     payload = {
         "prompt": prompt,
         "negative_prompt": negative_prompt,
@@ -75,8 +91,8 @@ def predict(txt, state, name, config):
         if config["enable_chatgpt"]:
             # predict the response
             messages=[
-                {"role": "system","content": config["system_prompt"]},
-                {"role": "user", "content": config["ai_nickname"] + "你好！"},
+                {"role": "system","content": presets[config["character_preset"]]["system_prompt"]},
+                {"role": "user", "content": presets[config["character_preset"]]["ai_nickname"] + "你好！"},
                 {"role": "assistant", "content": f"(<school uniform, waving at viewer, smiling, greeting, energetic>) 你好呀，{name}！"},
                 ]
             if len(state) <= max_interaction:
@@ -126,71 +142,80 @@ def predict(txt, state, name, config):
     state.append((txt, response_text))
     return state, state, img
 
-# set up the interface
-with gr.Blocks(title="ChatWaifu", css=additional_css) as main_block:
-    config = gr.State(value=yaml.load(open(config_path, "r", encoding='utf-8'), Loader=yaml.FullLoader))
-    with gr.Tabs(elem_id="tabs") as tabs:
-        with gr.TabItem(label="Chatting"):
-            title = gr.Markdown(notice)
-            state = gr.State([])
-            with gr.Row():
-                with gr.Column(scale=0.7):
-                    chat = gr.Chatbot(elem_id="chatbot")
-                    txt = gr.Textbox(show_label=False, placeholder="Type here...").style(container=False)
-                    # dream = gr.Textbox(show_label=False, placeholder="Only draw image...").style(container=False)
-            
-                with gr.Column(scale=0.3):
-                    name = gr.Textbox(label="Your name?", show_label=True, placeholder="想让AI怎么称呼你呢？").style(container=False)
-                    img = gr.Image(value=default_img, shape=(448, 640), show_label=False, interactive=False)
-            txt.submit(predict, [txt, state, name, config], [chat, state, img], show_progress=False, api_name="chatting")
-            txt.submit(lambda x: '', [txt], [txt], show_progress=False)
+def clear_state(state, chat):
+    return [], []
 
-        with gr.TabItem(label="Setting"):
-            title = gr.Markdown("**设置**")
-            button_apply_settings = gr.Button(value="Apply settings", variant='primary', elem_id="button_apply_settings")
-            with gr.Row():
-                with gr.Column(scale=0.5):
-                    checkbox_chatgpt = gr.Checkbox(value=config.value["enable_chatgpt"], label="Enable ChatGPT Response Pass", elem_id="checkbox_chatgpt", interactive=True)
-                    checkbox_diffusion = gr.Checkbox(value=config.value["enable_diffusion"], label="Enable Diffusion Image Pass", elem_id="checkbox_diffusion", interactive=True)
-                    checkbox_debug = gr.Checkbox(value=config.value["debug"], label="Enable Debug Mode", elem_id="checkbox_debug", interactive=True)
+if __name__ == "__main__":
+    # load presets
+    load_presets()
+    # set up the interface
+    with gr.Blocks(title="ChatWaifu", css=additional_css) as main_block:
+        config = gr.State(value=yaml.load(open(config_path, "r", encoding='utf-8'), Loader=yaml.FullLoader))
+        with gr.Tabs(elem_id="tabs") as tabs:
+            with gr.TabItem(label="Chatting"):
+                title = gr.Markdown(notice)
+                state = gr.State([])
+                with gr.Row():
+                    with gr.Column(scale=0.7):
+                        chat = gr.Chatbot(elem_id="chatbot")
+                        txt = gr.Textbox(show_label=False, placeholder="Type here...").style(container=False)
+                        # dream = gr.Textbox(show_label=False, placeholder="Only draw image...").style(container=False)
+                
+                    with gr.Column(scale=0.3):
+                        name = gr.Textbox(label="Your name?", show_label=True, placeholder="想让AI怎么称呼你呢？").style(container=False)
+                        img = gr.Image(value=default_img, shape=(448, 640), show_label=False, interactive=False)
+                txt.submit(predict, [txt, state, name, config], [chat, state, img], show_progress=False, api_name="chatting")
+                txt.submit(lambda x: '', [txt], [txt], show_progress=False)
+
+            with gr.TabItem(label="Setting"):
+                title = gr.Markdown("**设置**")
+                button_apply_settings = gr.Button(value="Apply settings", variant='primary', elem_id="button_apply_settings")
+                with gr.Row():
+                    with gr.Column(scale=0.5):
+                        dropdown_character = gr.Dropdown(value=config.value["character_preset"], label="Character", choices=list(presets.keys()), elem_id="dropdown_character", interactive=True)
+                        checkbox_chatgpt = gr.Checkbox(value=config.value["enable_chatgpt"], label="Enable ChatGPT Response Pass", elem_id="checkbox_chatgpt", interactive=True)
+                        checkbox_diffusion = gr.Checkbox(value=config.value["enable_diffusion"], label="Enable Diffusion Image Pass", elem_id="checkbox_diffusion", interactive=True)
+                        checkbox_debug = gr.Checkbox(value=config.value["debug"], label="Enable Debug Mode", elem_id="checkbox_debug", interactive=True)
+                        
+                        slider_steps = gr.Slider(value=config.value["diffusion_steps"], label="Diffusion Steps", minimum=10, maximum=50, step=1, elem_id="slider_steps", interactive=True)
+                        slider_cfg = gr.Slider(value=config.value["diffusion_cfg_scale"], label="Diffusion Cfg Scale", minimum=0, maximum=30, step=1, elem_id="slider_cfg", interactive=True)
+                        slider_width = gr.Slider(value=config.value["diffusion_width"], label="Diffusion Image Width", minimum=64, maximum=768, step=64, elem_id="slider_width", interactive=True)
+                        slider_height = gr.Slider(value=config.value["diffusion_height"], label="Diffusion Image Height", minimum=64, maximum=768, step=64, elem_id="slider_height", interactive=True)
+                        sampler_option = ['Euler a', 'Euler', 'LMS', 'Heun', 'DPM2', 'DPM2 a', 'DPM++ 2S a', 'DPM++ 2M', 'DPM++ SDE', 'DPM fast', 'DPM adaptive', 'LMS Karras', 'DPM2 Karras', 'DPM2 a Karras', 'DPM++ 2S a Karras', 'DPM++ 2M Karras', 'DPM++ SDE Karras', 'DDIM', 'PLMS']
+                        dropdown_sampler = gr.Dropdown(value=config.value["diffusion_sampler"], label="Diffusion Sampler", choices=sampler_option, elem_id="dropdown_sampler", interactive=True)
+
+                        # text_pos_prompt = gr.Textbox(value=config.value["diffusion_positive_prompt"], label="Diffusion Positive Prompt", show_label=True, elem_id="text_pos_prompt", interactive=True)
+                        # text_neg_prompt = gr.Textbox(value=config.value["diffusion_negative_prompt"], label="Diffusion Negative Prompt", show_label=True, elem_id="text_neg_prompt", interactive=True)
+                        # text_ai_nickname = gr.Textbox(value=config.value["ai_nickname"], label="AI Nickname", show_label=True, elem_id="text_ai_nickname", interactive=True)
+                        # text_sys_prompt = gr.Textbox(value=config.value["system_prompt"], label="ChatGPT System Prompt", show_label=True, elem_id="text_sys_prompt", interactive=True)
+
+                        slider_temperature = gr.Slider(value=config.value["chatgpt_temperature"], label="ChatGPT Temperature", minimum=0.0, maximum=1.0, step=0.1, elem_id="slider_temperature", interactive=True)
+
+
                     
-                    slider_steps = gr.Slider(value=config.value["diffusion_steps"], label="Diffusion Steps", minimum=10, maximum=50, step=1, elem_id="slider_steps", interactive=True)
-                    slider_cfg = gr.Slider(value=config.value["diffusion_cfg_scale"], label="Diffusion Cfg Scale", minimum=0, maximum=30, step=1, elem_id="slider_cfg", interactive=True)
-                    slider_width = gr.Slider(value=config.value["diffusion_width"], label="Diffusion Image Width", minimum=64, maximum=768, step=64, elem_id="slider_width", interactive=True)
-                    slider_height = gr.Slider(value=config.value["diffusion_height"], label="Diffusion Image Height", minimum=64, maximum=768, step=64, elem_id="slider_height", interactive=True)
-                    sampler_option = ['Euler a', 'Euler', 'LMS', 'Heun', 'DPM2', 'DPM2 a', 'DPM++ 2S a', 'DPM++ 2M', 'DPM++ SDE', 'DPM fast', 'DPM adaptive', 'LMS Karras', 'DPM2 Karras', 'DPM2 a Karras', 'DPM++ 2S a Karras', 'DPM++ 2M Karras', 'DPM++ SDE Karras', 'DDIM', 'PLMS']
-                    dropdown_sampler = gr.Dropdown(value=config.value["diffusion_sampler"], label="Diffusion Sampler", choices=sampler_option, elem_id="dropdown_sampler", interactive=True)
-                    text_pos_prompt = gr.Textbox(value=config.value["diffusion_positive_prompt"], label="Diffusion Positive Prompt", show_label=True, elem_id="text_pos_prompt", interactive=True)
-                    text_neg_prompt = gr.Textbox(value=config.value["diffusion_negative_prompt"], label="Diffusion Negative Prompt", show_label=True, elem_id="text_neg_prompt", interactive=True)
+                    with gr.Column(scale=0.5):
+                        json = gr.JSON(config.value, show_label=False, interactive=False)
 
-                    text_ai_nickname = gr.Textbox(value=config.value["ai_nickname"], label="AI Nickname", show_label=True, elem_id="text_ai_nickname", interactive=True)
-                    text_sys_prompt = gr.Textbox(value=config.value["system_prompt"], label="ChatGPT System Prompt", show_label=True, elem_id="text_sys_prompt", interactive=True)
-                    slider_temperature = gr.Slider(value=config.value["chatgpt_temperature"], label="ChatGPT Temperature", minimum=0.0, maximum=1.0, step=0.1, elem_id="slider_temperature", interactive=True)
+                    components = [(dropdown_character, "character_preset"),
+                                    (checkbox_chatgpt, "enable_chatgpt"),
+                                    (checkbox_diffusion, "enable_diffusion"),
+                                    (checkbox_debug, "debug"),
+                                    (slider_steps, "diffusion_steps"),
+                                    (slider_cfg, "diffusion_cfg_scale"),
+                                    (slider_width, "diffusion_width"),
+                                    (slider_height, "diffusion_height"),
+                                    (dropdown_sampler, "diffusion_sampler"),
+                                    # (text_pos_prompt, "diffusion_positive_prompt"),
+                                    # (text_neg_prompt, "diffusion_negative_prompt"),
+                                    # (text_ai_nickname, "ai_nickname"),
+                                    # (text_sys_prompt, "system_prompt"),
+                                    (slider_temperature, "chatgpt_temperature")]
+                    
+                    for component, key in components:
+                        component.change(lambda x, config, key=key: config.update({key: x}), [component, config], [], show_progress=False)
+                    dropdown_character.change(clear_state, [state, chat], [state, chat], show_progress=False)
 
-                
-                with gr.Column(scale=0.5):
-                    json = gr.JSON(config.value, show_label=False, interactive=False)
+                button_apply_settings.click(lambda x: x, [config], [json], show_progress=False)
 
-                components = [(checkbox_chatgpt, "enable_chatgpt"),
-                                (checkbox_diffusion, "enable_diffusion"),
-                                (checkbox_debug, "debug"),
-                                (slider_steps, "diffusion_steps"),
-                                (slider_cfg, "diffusion_cfg_scale"),
-                                (slider_width, "diffusion_width"),
-                                (slider_height, "diffusion_height"),
-                                (dropdown_sampler, "diffusion_sampler"),
-                                (text_pos_prompt, "diffusion_positive_prompt"),
-                                (text_neg_prompt, "diffusion_negative_prompt"),
-                                (text_ai_nickname, "ai_nickname"),
-                                (text_sys_prompt, "system_prompt"),
-                                (slider_temperature, "chatgpt_temperature")]
-                
-                for component, key in components:
-                    component.change(lambda x, config, key=key: config.update({key: x}), [component, config], [], show_progress=False)
-                
-            button_apply_settings.click(lambda x: x, [config], [json], show_progress=False)
-    
-                
-# demo.queue()
-main_block.launch(server_name="0.0.0.0")
-# demo.launch()
+    # Link Start!!!!!!!!
+    main_block.launch(server_name="0.0.0.0")
